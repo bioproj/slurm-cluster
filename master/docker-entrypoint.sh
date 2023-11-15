dd7174174232234aa66cf45a15f0aa9e31cf7b33
飞走的纸飞机 <1749748955@qq.com>
2023-11-15 09:18:49 +0000

    update

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
+# sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
+sudo service slurmd start
+sudo service mariadb start
+sudo mysql -u root < initialize-mariadb.sh
+sudo service slurmdbd start
 sudo service slurmctld start
-
 tail -f /dev/null
+# service  --status-all
+#  srun  hostname
+sudo -u admin sh start-slurmrestd.sh
