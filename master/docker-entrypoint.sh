11418171f4740f0fb0a4995fa06267187585049d
飞走的纸飞机 <1749748955@qq.com>
2023-11-24 13:37:11 +0000

    update

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
 # sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
-
+###################################################
+# 授权服务
 sudo service munge start
+###################################################
+
+###################################################
+# 执行节点
 sudo service slurmd start
+###################################################
+
+###################################################
+# 数据库配置
 sudo service mariadb start
 sudo mysql -u root < initialize-mariadb.sh
 sudo service slurmdbd start
-# sudo service slurmctld start
+###################################################
 
-# service  --status-all
-#  srun  hostname
-echo 'starting slurmrestd'
-sudo -u admin sh start-slurmrestd.sh &
-echo 'starting slurmctld'
+###################################################
+# 控制节点
 sleep 2
+echo 'starting slurmctld'
 sudo service slurmctld start
+###################################################
+
+###################################################
+# slurmrestd 配置，要运行slurmrestd必须加上参数--privileged 
+# docker run  --name slurm-all --privileged    --network=host --rm -it  wangyang1749/slurm-all:1.0
+# echo 'starting slurmrestd'
+# sudo -u admin sh start-slurmrestd.sh &
+###################################################
+sudo -u admin  jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' &
+
+echo "启动的服务..."
 service  --status-all
 # slurmctld -D
-
 tail -f /dev/null
+# docker build -t wangyang1749/slurm-all:1.0 .
+# docker run  --name slurm-all     --network=host --rm -it  wangyang1749/slurm-all:1.0
+# docker image push wangyang1749/slurm-all-master:1.0
